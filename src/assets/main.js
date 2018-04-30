00cc89da231824391b6cdbc0c57bd8bb4690f86b
r35yan <r35yan@edu.uwaterloo.ca>
2018-04-30 13:25:15 -0400

    Project solved

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/najibk.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log('response', response.courses.completed);
+      response.courses.completed.forEach(function(item, index){
+      	$course = $('<div />', {'class': 'course'}).appendTo($('#badges'));
+      	$('<h3 />', {text: item.title}).appendTo($course);
+        $('<img />', {src: item.badge}).appendTo($course)
+      	$('<a />', {href: item.url, target: '_blank', 'class': "btn btn-primary", text: 'See Course'}).appendTo($course);
+
+	  });
+      
+    }
+  });
 
 });
